8595d8198c9d5075079c876f780fdff0d20c32ae
gknows <gknows@qq.com>
2016-08-03 17:12:24 +0800

    delete test code

--- SamChat/Classes/DataBase/SAMCDBBase.m
+++ SamChat/Classes/DataBase/SAMCDBBase.m
@@ -32,17 +32,6 @@
     NSString *filepath = [[NTESFileLocationHelper userDirectory] stringByAppendingString:name];
     DDLogDebug(@"filepath %@", filepath);
     self.queue = [FMDatabaseQueue databaseQueueWithPath:filepath];
-    [self.queue inDatabase:^(FMDatabase *db) {
-        NSArray *sqls = @[@"create table if not exists notifications(serial integer primary key, \
-                          timetag integer,sender text,receiver text,content text,status integer)",
-                          @"create index if not exists readindex on notifications(status)",
-                          @"create index if not exists timetagindex on notifications(timetag)"];
-        for (NSString *sql in sqls) {
-            if (![db executeUpdate:sql]) {
-                DDLogError(@"error: execute sql %@ failed error %@",sql,db.lastError);
-            }
-        }
-    }];
 }
 
 @end
