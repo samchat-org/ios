8a748c4fa0568ba52d19f4f3822da4f9f06c4e1e
gknows <gknows@qq.com>
2016-08-24 10:09:05 +0800

    register GeTuiSdk and implement dummy GeTuiSdkDelegate

--- SamChat/SAMCAppDelegate.m
+++ SamChat/SAMCAppDelegate.m
@@ -28,10 +28,16 @@
 #import "SAMCChatManager.h"
 #import "SAMCPreferenceManager.h"
 #import "SAMCAccountManager.h"
+#import "GeTuiSdk.h"
 
 NSString *NTESNotificationLogout = @"NTESNotificationLogout";
 NSString * const SAMCLoginNotification = @"SAMCLoginNotification";
-@interface SAMCAppDelegate ()<SAMCLoginManagerDelegate>
+
+#define SAMCGeTuiAppId        @"e56px9TMay6OJRDrwE21P9"
+#define SAMCGeTuiAppKey       @"PUnNqMKGxaAdRoWFDmaTX5"
+#define SAMCGeTuiAppSecret    @"ovHqi2uWuw5pvqYJf5QgO6"
+
+@interface SAMCAppDelegate ()<SAMCLoginManagerDelegate,GeTuiSdkDelegate>
 @property (nonatomic,strong) NTESSDKConfig *config;
 @end
 
@@ -58,6 +64,9 @@ NSString * const SAMCLoginNotification = @"SAMCLoginNotification";
                                   cerName:cerName];
     
     [NIMCustomObject registerCustomDecoder:[NTESCustomAttachmentDecoder new]];
+    
+    // 个推注册
+    [GeTuiSdk startSdkWithAppId:SAMCGeTuiAppId appKey:SAMCGeTuiAppKey appSecret:SAMCGeTuiAppSecret delegate:self];
     
     
     [self setupServices];
@@ -105,8 +114,15 @@ NSString * const SAMCLoginNotification = @"SAMCLoginNotification";
 
 - (void)application:(UIApplication *)app didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
 {
+    // 云信DeviceToken上传
     [[NIMSDK sharedSDK] updateApnsToken:deviceToken];
     DDLogInfo(@"didRegisterForRemoteNotificationsWithDeviceToken:  %@", deviceToken);
+    
+    // 个推DeviceToken上传
+    NSString *token = [[deviceToken description] stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"<>"]];
+    token = [token stringByReplacingOccurrencesOfString:@" " withString:@""];
+    DDLogInfo(@"\n>>>[DeviceToken Success]:%@\n\n", token);
+    [GeTuiSdk registerDeviceToken:token];
 }
 
 - (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo{
@@ -245,6 +261,58 @@ NSString * const SAMCLoginNotification = @"SAMCLoginNotification";
 {
     [[NTESLogManager sharedManager] start];
     [[NTESNotificationCenter sharedCenter] start];
+}
+
+#pragma mark - GeTuiSdkDelegate
+/** SDK启动成功返回cid */
+- (void)GeTuiSdkDidRegisterClient:(NSString *)clientId {
+    // [4-EXT-1]: 个推SDK已注册，返回clientId
+    DDLogDebug(@"\n>>>[GeTuiSdk RegisterClient]:%@\n\n", clientId);
+}
+
+/** SDK遇到错误回调 */
+- (void)GeTuiSdkDidOccurError:(NSError *)error {
+    // [EXT]:个推错误报告，集成步骤发生的任何错误都在这里通知，如果集成后，无法正常收到消息，查看这里的通知。
+    DDLogDebug(@"\n>>>[GexinSdk error]:%@\n\n", [error localizedDescription]);
+}
+
+/** SDK收到透传消息回调 */
+- (void)GeTuiSdkDidReceivePayloadData:(NSData *)payloadData
+                            andTaskId:(NSString *)taskId
+                             andMsgId:(NSString *)msgId
+                           andOffLine:(BOOL)offLine
+                          fromGtAppId:(NSString *)appId {
+    
+    // [4]: 收到个推消息
+    NSString *payloadMsg = nil;
+    if (payloadData) {
+        payloadMsg = [[NSString alloc] initWithBytes:payloadData.bytes length:payloadData.length encoding:NSUTF8StringEncoding];
+    }
+    
+    NSString *msg = [NSString stringWithFormat:@"taskId=%@,messageId:%@,payloadMsg:%@%@", taskId, msgId, payloadMsg, offLine ? @"<离线消息>" : @""];
+    DDLogDebug(@"\n>>>[GexinSdk ReceivePayload]:%@\n\n", msg);
+}
+
+/** SDK收到sendMessage消息回调 */
+- (void)GeTuiSdkDidSendMessage:(NSString *)messageId result:(int)result {
+    // [4-EXT]:发送上行消息结果反馈
+    NSString *msg = [NSString stringWithFormat:@"sendmessage=%@,result=%d", messageId, result];
+    DDLogDebug(@"\n>>>[GexinSdk DidSendMessage]:%@\n\n", msg);
+}
+
+/** SDK运行状态通知 */
+- (void)GeTuiSDkDidNotifySdkState:(SdkStatus)aStatus {
+    // [EXT]:通知SDK运行状态
+    DDLogDebug(@"\n>>>[GexinSdk SdkState]:%u\n\n", aStatus);
+}
+
+/** SDK设置推送模式回调 */
+- (void)GeTuiSdkDidSetPushMode:(BOOL)isModeOff error:(NSError *)error {
+    if (error) {
+        NSLog(@"\n>>>[GexinSdk SetModeOff Error]:%@\n\n", [error localizedDescription]);
+        return;
+    }
+    DDLogDebug(@"\n>>>[GexinSdk SetModeOff]:%@\n\n", isModeOff ? @"开启" : @"关闭");
 }
 
 
