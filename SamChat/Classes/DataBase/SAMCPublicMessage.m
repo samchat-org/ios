c9c3b24a225d33947017f211163586fd94a8125c
gknows <gknows@qq.com>
2016-09-02 17:13:23 +0800

    move SAMCPublicMessage to database model group

---
+++ SamChat/Classes/DataBase/SAMCPublicMessage.m
@@ -0,0 +1,26 @@
+//
+//  SAMCPublicMessage.m
+//  SamChat
+//
+//  Created by HJ on 9/2/16.
+//  Copyright © 2016 SamChat. All rights reserved.
+//
+
+#import "SAMCPublicMessage.h"
+
+@implementation SAMCPublicMessage
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        self.messageType = NIMMessageTypeText;
+        self.deliveryState = NIMMessageDeliveryStateDelivering;
+        self.isReceivedMsg = NO;
+        self.isOutgoingMsg = YES;
+        self.isRemoteRead = YES;
+    }
+    return self;
+}
+
+@end
