5ced3637b90ba331ddd29f24b5f73f7d07ad5624
gknows <gknows@qq.com>
2016-09-20 20:38:18 +0800

    add get places info api and delete useless file

---
+++ SamChat/Classes/Sections/Base/Model/SAMCPlaceInfo.h
@@ -0,0 +1,13 @@
+//
+//  SAMCPlaceInfo.h
+//  SamChat
+//
+//  Created by HJ on 9/20/16.
+//  Copyright © 2016 SamChat. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SAMCPlaceInfo : NSObject
+
+@end
---
+++ SamChat/Classes/Sections/Base/Model/SAMCPlaceInfo.m
@@ -0,0 +1,13 @@
+//
+//  SAMCPlaceInfo.m
+//  SamChat
+//
+//  Created by HJ on 9/20/16.
+//  Copyright © 2016 SamChat. All rights reserved.
+//
+
+#import "SAMCPlaceInfo.h"
+
+@implementation SAMCPlaceInfo
+
+@end
