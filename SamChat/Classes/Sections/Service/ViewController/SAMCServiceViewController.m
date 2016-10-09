7acbb80635bdc9658fb99f8ab1f7e19d6d1db423
gknows <gknows@qq.com>
2016-10-09 20:18:14 +0800

    delete tableView init in sp mode of service vc, use lazy load

--- SamChat/Classes/Sections/Service/ViewController/SAMCServiceViewController.m
+++ SamChat/Classes/Sections/Service/ViewController/SAMCServiceViewController.m
@@ -64,39 +64,6 @@
     
     [self setupCustomModeEmptyRequestViews];
     [self setupCustomModeNotEmptyRequestViews];
-    
-//    self.requestButton = [[UIButton alloc] init];
-//    self.requestButton.backgroundColor = [UIColor grayColor];
-//    [self.requestButton.layer setCornerRadius:6.0f];
-//    self.requestButton.translatesAutoresizingMaskIntoConstraints = NO;
-//    [self.requestButton setTitle:@"+ Make a new service request" forState:UIControlStateNormal];
-//    [self.requestButton addTarget:self action:@selector(touchMakeNewRequest:) forControlEvents:UIControlEventTouchUpInside];
-//    [self.view addSubview:self.requestButton];
-//    
-//    self.tableView = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStyleGrouped];
-//    self.tableView.backgroundColor = [UIColor greenColor];
-//    self.tableView.translatesAutoresizingMaskIntoConstraints = NO;
-//    self.tableView.delegate = self.delegator;
-//    self.tableView.dataSource = self.delegator;
-//    [self.view addSubview:self.tableView];
-//    
-//    [self.tableView setSeparatorInset:UIEdgeInsetsZero];
-//    if ([self.tableView respondsToSelector:@selector(setLayoutMargins:)]) {
-//        [self.tableView setLayoutMargins:UIEdgeInsetsZero];
-//    }
-//    
-//    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-20-[_requestButton]-20-|"
-//                                                                      options:0
-//                                                                      metrics:nil
-//                                                                        views:NSDictionaryOfVariableBindings(_requestButton)]];
-//    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[_tableView]|"
-//                                                                      options:0
-//                                                                      metrics:nil
-//                                                                        views:NSDictionaryOfVariableBindings(_tableView)]];
-//    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-20-[_requestButton(44)]-20-[_tableView]|"
-//                                                                      options:0
-//                                                                      metrics:nil
-//                                                                        views:NSDictionaryOfVariableBindings(_requestButton, _tableView)]];
 }
 
 - (void)setupCustomModeEmptyRequestViews
@@ -174,18 +141,7 @@
     self.tableView = nil;
     self.requestButton = nil;
     self.navigationItem.title = @"Service Requests";
-    
-    self.tableView = [[UITableView alloc] init];
-    self.tableView.backgroundColor = [UIColor yellowColor];
-    self.tableView.translatesAutoresizingMaskIntoConstraints = NO;
-    self.tableView.delegate = self.delegator;
-    self.tableView.dataSource = self.delegator;
     [self.view addSubview:self.tableView];
-    
-    [self.tableView setSeparatorInset:UIEdgeInsetsZero];
-    if ([self.tableView respondsToSelector:@selector(setLayoutMargins:)]) {
-        [self.tableView setLayoutMargins:UIEdgeInsetsZero];
-    }
     
     [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[_tableView]|"
                                                                       options:0
