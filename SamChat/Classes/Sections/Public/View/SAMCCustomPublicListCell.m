bc8572972758acc368a8177ce60ef233cdf39b17
gknows <gknows@qq.com>
2016-10-24 19:03:30 +0800

    update constraint of SAMCCustomPublicListCell

--- SamChat/Classes/Sections/Public/View/SAMCCustomPublicListCell.m
+++ SamChat/Classes/Sections/Public/View/SAMCCustomPublicListCell.m
@@ -52,7 +52,7 @@
                                                                toItem:nil
                                                             attribute:NSLayoutAttributeNotAnAttribute
                                                            multiplier:0.0f
-                                                             constant:60.0f]];
+                                                             constant:50.0f]];
     [self addConstraint:[NSLayoutConstraint constraintWithItem:_avatarView
                                                      attribute:NSLayoutAttributeCenterY
                                                      relatedBy:NSLayoutRelationEqual
@@ -88,14 +88,14 @@
                                                         toItem:self
                                                      attribute:NSLayoutAttributeTop
                                                     multiplier:1.0f
-                                                      constant:12.0f]];
+                                                      constant:8.0f]];
     [self addConstraint:[NSLayoutConstraint constraintWithItem:_messageLabel
                                                      attribute:NSLayoutAttributeTop
                                                      relatedBy:NSLayoutRelationEqual
                                                         toItem:_nameLabel
                                                      attribute:NSLayoutAttributeBottom
                                                     multiplier:1.0f
-                                                      constant:8.0f]];
+                                                      constant:0.0f]];
     [_nameLabel setContentHuggingPriority:UILayoutPriorityDefaultHigh forAxis:UILayoutConstraintAxisHorizontal];
     [_categoryLabel setContentHuggingPriority:UILayoutPriorityDefaultHigh forAxis:UILayoutConstraintAxisHorizontal];
     [_categoryLabel setContentCompressionResistancePriority:UILayoutPriorityDefaultLow forAxis:UILayoutConstraintAxisHorizontal];
