c472438df6c0d03066e5d67a80fb7b69fda959b4
gknows <gknows@qq.com>
2016-11-03 10:21:13 +0800

    add isValidCellphone: to SAMCUtils

---
+++ SamChat/Classes/Util/SAMCUtils.h
@@ -0,0 +1,15 @@
+//
+//  SAMCUtils.h
+//  SamChat
+//
+//  Created by HJ on 11/2/16.
+//  Copyright © 2016 SamChat. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SAMCUtils : NSObject
+
++ (BOOL)isValidCellphone:(NSString *)cellphone;
+
+@end
---
+++ SamChat/Classes/Util/SAMCUtils.m
@@ -0,0 +1,25 @@
+//
+//  SAMCUtils.m
+//  SamChat
+//
+//  Created by HJ on 11/2/16.
+//  Copyright © 2016 SamChat. All rights reserved.
+//
+
+#import "SAMCUtils.h"
+
+@implementation SAMCUtils
+
++ (BOOL)isValidCellphone:(NSString *)cellphone
+{
+    if ((cellphone.length<5) || (cellphone.length>11)) {
+        return false;
+    }
+    cellphone = [cellphone stringByTrimmingCharactersInSet:[NSCharacterSet decimalDigitCharacterSet]];
+    if (cellphone.length > 0) {
+        return false;
+    }
+    return true;
+}
+
+@end
