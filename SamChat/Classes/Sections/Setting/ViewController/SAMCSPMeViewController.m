03d6f4ac1a92d718be1c30f87e16b444dbe3cff7
gknows <gknows@qq.com>
2016-11-11 19:56:15 +0800

    delete footerview of sp me vc

--- SamChat/Classes/Sections/Setting/ViewController/SAMCSPMeViewController.m
+++ SamChat/Classes/Sections/Setting/ViewController/SAMCSPMeViewController.m
@@ -14,7 +14,6 @@
 #import "SAMCUserManager.h"
 #import "SAMCUnreadCountManager.h"
 #import "SAMCQRCodeScanViewController.h"
-#import "SAMCFooterView.h"
 
 @interface SAMCSPMeViewController ()<UITableViewDelegate, UITableViewDataSource, SAMCUserManagerDelegate, SAMCUnreadCountManagerDelegate>
 
@@ -134,16 +133,6 @@
 }
 
 #pragma mark - UITableViewDataSource
-- (UIView *)tableView:(UITableView *)tableView viewForFooterInSection:(NSInteger)section
-{
-    if (section != 2) {
-        return nil;
-    }
-    SAMCFooterView *footerView = [[SAMCFooterView alloc] init];
-    footerView.textLabel.text = @"Enable or disable SamChat Notifications via \"Settings\"->\"Notifications\" on your iPhone.";
-    return footerView;
-}
-
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
     return 3;
